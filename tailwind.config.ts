1aa11d8e45b7f419adb1b433bebc0f0153731c7d
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-07-29 02:52:45 +0900

    :label: (tailwindcss.d.ts) Add types of tailwindcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,13 +4,15 @@
  ** Docs: https://tailwindcss.com/docs/configuration
  ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
  */
-module.exports = {
+import { TailwindcssConfiguration } from '@/src/types/tailwindcss'
+const config: TailwindcssConfiguration = {
   theme: {},
   variants: {},
   plugins: [],
   purge: {
     // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
     enabled: process.env.NODE_ENV === 'production',
-    content: ['components/**/*.vue', 'layouts/**/*.vue', 'pages/**/*.vue', 'plugins/**/*.js', 'nuxt.config.js']
+    content: ['components/**/*.vue', 'layouts/**/*.vue', 'pages/**/*.vue', 'plugins/**/*.ts', 'nuxt.config.ts']
   }
 }
+module.exports = config
