d28ac3d9d2ee179d538e6dcd537ef2778072b118
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-07-30 01:07:11 +0900

    :sparkles: (InputSearch.vue) Add input search element

---
+++ src/components/molecules/InputSearch.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <input
+      class="py-2 px-3 border truncate leading-5 rounded-md focus:border-grey-300"
+      type="search"
+      placeholder="Search"
+      autocomplete="off"
+    />
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
