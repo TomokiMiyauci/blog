c4d3dc8701766fe55a03be9f95c0057cc99918a5
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-07-30 02:19:28 +0900

    :wrench: (tailwind.config.ts) Resolve relative path

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,13 @@
  ** Docs: https://tailwindcss.com/docs/configuration
  ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
  */
+import { join } from 'path'
+
 import { TailwindcssConfiguration } from '@/src/types/tailwindcss'
+
+const BASE_DIR = join(__dirname, 'src')
+const VUE_FILE = join('**', '*.vue')
+
 const config: TailwindcssConfiguration = {
   theme: {},
   variants: {},
@@ -12,7 +18,13 @@ const config: TailwindcssConfiguration = {
   purge: {
     // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
     enabled: process.env.NODE_ENV === 'production',
-    content: ['components/**/*.vue', 'layouts/**/*.vue', 'pages/**/*.vue', 'plugins/**/*.ts', 'nuxt.config.ts']
+    content: [
+      join(BASE_DIR, 'components', VUE_FILE),
+      join(BASE_DIR, 'layouts', VUE_FILE),
+      join(BASE_DIR, 'pages', VUE_FILE),
+      join(BASE_DIR, 'plugins', '**', '*.ts'),
+      join(__dirname, 'nuxt.config.ts')
+    ]
   }
 }
 module.exports = config
