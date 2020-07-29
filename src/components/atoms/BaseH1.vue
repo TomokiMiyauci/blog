ef45a3baf0eb531c297c54fbceef54926924864f
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-07-30 01:06:44 +0900

    :sparkles: (BaseH1.vue) Add base h1 element

---
+++ src/components/atoms/BaseH1.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 class="font-mono text-4xl font-semibold mb-4 leading-tight">{{ text }}</h1>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'nuxt-composition-api'
+  export default defineComponent({
+    props: {
+      text: {
+        type: String,
+        default: ''
+      }
+    }
+  })
+</script>
