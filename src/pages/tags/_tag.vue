2bd74f3a437a845359f9ac5b3a03e87c593487bb
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-07-30 23:43:09 +0900

    :triangular_flag_on_post: (/tags) Add tags page

---
+++ src/pages/tags/_tag.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <article-headline v-for="article in articles" :key="article.id" :headline="article" />
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'nuxt-composition-api'
+  export default defineComponent({
+    async asyncData({ $content, params }) {
+      const articles = await $content('articles')
+        .where({ tags: { $in: params.tag } })
+        .fetch()
+
+      return { articles }
+    }
+  })
+</script>
