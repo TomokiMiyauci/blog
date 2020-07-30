a619a778f50171fa94f1e8d5f0aa2f6161cda313
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-07-30 23:41:08 +0900

    :sparkles: Add article components

---
+++ src/components/molecules/ArticleHeadline.vue
@@ -0,0 +1,23 @@
+<template>
+  <nuxt-link tag="article" to="/h" class="bg-blue p-4 m-4 border rounded cursor-pointer">
+    <h2>{{ headline.title }}</h2>
+    <p>{{ headline.description }}</p>
+  </nuxt-link>
+</template>
+
+<script lang="ts">
+  // eslint-disable-next-line import/named
+  import { defineComponent, PropType } from 'nuxt-composition-api'
+  type Headline = {
+    title: string
+    description: string
+  }
+  export default defineComponent({
+    props: {
+      headline: {
+        type: Object as PropType<Headline>,
+        required: true
+      }
+    }
+  })
+</script>
---
+++ src/components/molecules/TagsList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>Tags</h2>
+    <base-tag v-for="tag in tags" :key="tag" :name="tag" />
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'nuxt-composition-api'
+  export default defineComponent({
+    props: {
+      tags: {
+        type: Array,
+        default: () => []
+      }
+    }
+  })
+</script>
