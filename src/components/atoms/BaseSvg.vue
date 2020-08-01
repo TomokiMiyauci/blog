2224d74f447d099f374e56f96bfba14ee89192f4
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-08-01 11:53:18 +0900

    :sparkles: (BaseSvg.vue) Add base svg component

---
+++ src/components/atoms/BaseSvg.vue
@@ -0,0 +1,38 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="width"
+    :height="height"
+    :viewBox="`0 0 ${width} ${height}`"
+    :aria-labelledby="iconName"
+    role="presentation"
+  >
+    <title lang="en">{{ iconName }} icon</title>
+    <g :fill="iconColor">
+      <slot />
+    </g>
+  </svg>
+</template>
+
+<script lang="ts">
+  export default {
+    props: {
+      iconName: {
+        type: String,
+        default: 'box'
+      },
+      width: {
+        type: [Number, String],
+        default: 24
+      },
+      height: {
+        type: [Number, String],
+        default: 24
+      },
+      iconColor: {
+        type: String,
+        default: 'currentColor'
+      }
+    }
+  }
+</script>
