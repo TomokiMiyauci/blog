c1af5fe7f7eaf0c6e309a0c10c4f1308f532b366
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-08-03 01:02:31 +0900

    :recycle: (ArticleHeadline.vue) Change to option of args

--- src/components/molecules/ArticleHeadline.vue
+++ src/components/molecules/ArticleHeadline.vue
@@ -1,21 +1,22 @@
 <template>
-  <nuxt-link tag="article" to="/h" class="bg-blue p-4 m-4 border rounded cursor-pointer">
+  <nuxt-link
+    tag="article"
+    :to="{ name: 'slug', params: { slug: headline.slug } }"
+    class="bg-blue p-4 m-4 border rounded cursor-pointer"
+  >
     <h2>{{ headline.title }}</h2>
     <p>{{ headline.description }}</p>
   </nuxt-link>
 </template>
 
 <script lang="ts">
-  // eslint-disable-next-line import/named
-  import { defineComponent, PropType } from 'nuxt-composition-api'
-  type Headline = {
-    title: string
-    description: string
-  }
+  import { Headline } from '@/types/article'
+  import { defineComponent } from 'nuxt-composition-api'
+
   export default defineComponent({
     props: {
       headline: {
-        type: Object as PropType<Headline>,
+        type: Object as () => Headline,
         required: true
       }
     }
