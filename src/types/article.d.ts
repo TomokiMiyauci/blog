2d6ac84522060e6c3e05c0ccb7dcccdc7985ec56
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-08-06 02:52:27 +0900

    :label: (article.d.ts) Add base content and stats

--- src/types/article.d.ts
+++ src/types/article.d.ts
@@ -1,9 +1,30 @@
-export interface Article {
+type Stats = {
+  text: string
+  minutes: number
+  time: number
+  words: number
+}
+
+type BaseContent = {
+  dir: string
+  path: string
+  slug: string
+  extension: '.md'
+  createdAt: Date
+  updated: Date
+}
+
+type Frontmatter = {
   title: string
   description: string
-  slug: string
+  tags: string[]
 }
+
+export type Article = BaseContent &
+  Frontmatter & {
+    readingTime: Stats['text']
+  }
 
 export type PrevNext = Pick<Article, 'title' | 'slug'>
 
-export type Headline = Pick<Article, 'title' | 'description' | 'slug'>
+export type Headline = Pick<Article, 'title' | 'description' | 'slug' | 'readingTime' | 'createdAt'>
