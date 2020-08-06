9ac24c53c73a15b8dc5e99d614a4fe359434f5a1
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-08-07 01:40:13 +0900

    :lipstick: (ArticleHeadline.vue) Apply responsive

--- src/components/molecules/ArticleHeadline.vue
+++ src/components/molecules/ArticleHeadline.vue
@@ -2,22 +2,21 @@
   <nuxt-link
     tag="article"
     :to="{ name: 'slug', params: { slug: headline.slug } }"
-    class="bg-gray-100 p-4 m-4 rounded-md flex shadow-md cursor-pointer hover:bg-gray-300 hover:shadow-xl transition duration-300 ease-linear"
+    class="bg-gray-100 p-4 m-4 rounded-md flex flex-col-reverse lg:flex-row shadow-md cursor-pointer hover:bg-gray-300 hover:shadow-xl transition duration-300 ease-linear"
   >
-    <div class="lg:w-2/3">
+    <div class="lg:w-2/3 mx-2">
       <base-nuxt-link class="text-2xl font-bold" :to="{ name: 'slug', params: { slug: headline.slug } }">
         {{ headline.title }}
       </base-nuxt-link>
       <p class="mt-5 mb-3">{{ headline.description }}</p>
       <component :is="i" />
 
-      <h2 class="mt-5">
-        <mdi-calendar-edit class="mr-1" />{{ formatDate(headline.createdAt) }} <mdi-timer class="mr-1" />{{
-          headline.readingTime
-        }}
+      <h2 class="mt-5 flex justify-between">
+        <span><mdi-calendar-edit class="mr-1" />{{ formatDate(headline.createdAt) }}</span>
+        <span> <mdi-timer class="mr-1" />{{ headline.readingTime }}</span>
       </h2>
     </div>
-    <div class="w-full bg-green-200 rounded shadow flex justify-center items-center lg:w-1/3">
+    <div class="w-full p-4 bg-green-200 rounded shadow flex justify-center items-center lg:w-1/3">
       <nuxt-icon height="100" />
     </div>
   </nuxt-link>
