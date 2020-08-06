f4c219d0bb1e1bab081785655565e83430321de8
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-08-07 01:42:39 +0900

    :recycle: (default.vue) Remove unused

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -2,9 +2,14 @@
   <div>
     <app-bar />
     <main class="pt-16 lg:pt-24 transition-colors duration-300 ease-linear">
-      <div class="block md:grid max-w-screen-xl mx-auto grid-temp">
-        <Nuxt style="grid-column: 2 / 3;" />
-        <tags-list style="grid-column: 3 / 3;" :tags="['hello', 'world', 'blog']" />
+      <div class="max-w-screen-sm mx-auto px-4 sm:px-6 md:px-8 pt-12 pb-20 flex flex-col items-center">
+        <h1 class="font-extrabold text-5xl leading-10 text-teal-900"><span>T</span>echsrc</h1>
+        <h2 class="mt-4 sm:mt-5 font-light text-2xl text-center leading-tight text-gray-900">
+          Blog for Enginner
+        </h2>
+      </div>
+      <div class="block max-w-screen-xl mx-auto">
+        <Nuxt />
       </div>
     </main>
   </div>
@@ -13,10 +18,3 @@
 <script>
   export default {}
 </script>
-
-<style scoped>
-  .grid-temp {
-    grid-template-columns: 240px 1fr 240px;
-    width: calc(100% - 50px);
-  }
-</style>
