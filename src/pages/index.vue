e8530d9d79595b80a06897721ffb7c665c8eb21d
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-08-07 01:44:02 +0900

    :sparkles: (index.vue) Apply grid style

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,12 @@
 <template>
-  <section>
-    <article-headline v-for="article in articles" :key="article.id" :headline="article" />
-  </section>
+  <div class="flex flex-col justify-center lg:grid grid-temp">
+    <section style="grid-column: 2 / 3;">
+      <article-headline v-for="article in articles" :key="article.id" :headline="article" />
+    </section>
+    <div style="grid-column: 3 / 3;" class="p-4">
+      <tags-list class="lg:sticky lg:top-0 lg:pt-24 lg:-mt-24" :tags="['hello', 'world', 'blog']" />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -9,10 +14,16 @@
   import { defineComponent } from 'nuxt-composition-api'
   export default defineComponent({
     async asyncData({ $content }) {
-      const articles = await $content('articles')
-        .only(['title', 'description', 'slug', 'tags', 'readingTime', 'createdAt'])
-        .fetch<Headline>()
+      const headline: (keyof Headline)[] = ['title', 'description', 'slug', 'tags', 'readingTime', 'createdAt']
+      const articles = await $content('articles').only(headline).fetch<Headline>()
+
       return { articles }
     }
   })
 </script>
+
+<style scoped>
+  .grid-temp {
+    grid-template-columns: 340px 1fr 240px;
+  }
+</style>
