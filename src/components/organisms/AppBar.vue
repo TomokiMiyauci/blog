bea5fdd148f09ecd4a4fbe65493d4071b8fa3ddc
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-08-07 01:40:57 +0900

    :sparkles: (AppBar.vue) Add logo

--- src/components/organisms/AppBar.vue
+++ src/components/organisms/AppBar.vue
@@ -3,7 +3,12 @@
     class="fixed top-0 bg-white left-0 right-0 z-30 bg-light-elevatedSurface dark:bg-dark-elevatedSurface border-b border-light-border dark:border-dark-border lg:border-0 h-16 lg:h-24 transition-colors duration-300 ease-linear"
   >
     <div class="container relative h-full flex items-center justify-between mx-auto px-4 lg:py-6">
-      <nuxt-link class="text-4xl font-mono" to="/">Techxas</nuxt-link>
+      <nuxt-link class="font-mono inline-flex items-end" to="/">
+        <img width="56px" src="/icon.png" />
+        <span class="ml-2 text-3xl leading-none tracking-tighter lg:text-4xl text-teal-900"
+          ><span style="color: #72deaf;">T</span>ech<span style="color: #72deaf;">s</span>rc</span
+        ></nuxt-link
+      >
       <fulltext-search />
     </div>
   </header>
