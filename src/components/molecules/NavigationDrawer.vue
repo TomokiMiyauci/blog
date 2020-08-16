f1af408338621361b3969a9c012e3f49123ce70e
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-08-17 01:03:47 +0900

    :sparkles: (NavigationDrawer.vue) Add navigation

---
+++ src/components/molecules/NavigationDrawer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="absolute px-3 flex items-center inset-0 bg-white dark:bg-gray-800 md:hidden">
+    <lang-switcher />
+    <button-color-mode />
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'nuxt-composition-api'
+  export default defineComponent({})
+</script>
