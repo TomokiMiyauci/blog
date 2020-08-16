fe77369e622b731c79999df1cf74c15dd6465d28
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-08-16 16:42:27 +0900

    :sparkles: (outside-click.ts) Add custom directive of outside click

---
+++ src/directives/outside-click.ts
@@ -0,0 +1,33 @@
+import { DirectiveOptions, DirectiveFunction } from 'vue'
+import { DirectiveBinding } from 'vue/types/options'
+
+const validate = (binding: DirectiveBinding): boolean => {
+  if (typeof binding.value !== 'function') {
+    // eslint-disable-next-line no-console
+    console.warn('Directive[click-outside]: provided expression', binding.expression, 'is not a function.')
+    return false
+  }
+
+  return true
+}
+
+const bind: DirectiveFunction = (el: HTMLElement, binding: DirectiveBinding): void => {
+  if (!validate(binding)) return
+  el.onclick = (e) => {
+    e.stopPropagation()
+  }
+
+  document.addEventListener(
+    'click',
+    () => {
+      binding.value()
+    },
+    { once: true }
+  )
+}
+
+const OutsideClickDirective: DirectiveOptions = {
+  bind
+}
+
+export default OutsideClickDirective
