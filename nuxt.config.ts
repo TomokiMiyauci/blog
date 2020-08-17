e1451d9b6ba9d11b9d6d9af873c59584da7a2fd7
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-08-18 00:44:40 +0900

    :wrench: (nuxt.config.ts) Enable firestore

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -131,20 +131,21 @@ const config: NuxtConfig = {
 
   firebase: {
     config: {
-      apiKey: process.env.API_KEY,
-      authDomain: process.env.AUTH_DOMAIN,
-      databaseURL: process.env.DATABASE_URL,
-      projectId: process.env.PROJECT_ID,
-      storageBucket: process.env.STORAGE_BUCKET,
-      messagingSenderId: process.env.MESSAGING_SENDER_ID,
-      appId: process.env.APP_ID,
-      measurementId: process.env.MEASUREMENT_ID
+      apiKey: process.env.API_KEY!,
+      authDomain: process.env.AUTH_DOMAIN!,
+      databaseURL: process.env.DATABASE_URL!,
+      projectId: process.env.PROJECT_ID!,
+      storageBucket: process.env.STORAGE_BUCKET!,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID!,
+      appId: process.env.APP_ID!,
+      measurementId: process.env.MEASUREMENT_ID!
     },
     onFirebaseHosting: true,
     services: {
       analytics: {
         collectionEnabled: isProduction
-      }
+      },
+      firestore: true
     }
   },
   /*
@@ -211,7 +212,7 @@ const config: NuxtConfig = {
     }
   },
 
-  // modern: isProduction ? 'client' : false,
+  modern: isProduction ? 'client' : false,
 
   features: {
     store: false,
