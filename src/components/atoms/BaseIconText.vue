6f802ae7397730d7963e26746e138804eb2869a4
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-08-18 00:47:59 +0900

    :sparkles: Add new icons and icon with text component

---
+++ src/components/atoms/BaseIconText.vue
@@ -0,0 +1,18 @@
+<template>
+  <span>
+    <slot />
+    <span class="ml-1">{{ text }}</span>
+  </span>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'nuxt-composition-api'
+  export default defineComponent({
+    props: {
+      text: {
+        type: [String, Number],
+        default: ''
+      }
+    }
+  })
+</script>
