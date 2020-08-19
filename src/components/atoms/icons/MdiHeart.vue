472c4fe5d03cb6f1ac06f486bd7f45acc45f1881
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-08-19 20:06:43 +0900

    :sparkles: Add new icon and favorite button

---
+++ src/components/atoms/icons/MdiHeart.vue
@@ -0,0 +1,16 @@
+<template>
+  <lazy-base-svg v-bind="$attrs">
+    <path :d="mdiHeart" />
+  </lazy-base-svg>
+</template>
+
+<script lang="ts">
+  import { mdiHeart } from '@mdi/js'
+  import { defineComponent } from 'nuxt-composition-api'
+
+  export default defineComponent({
+    setup() {
+      return { mdiHeart }
+    }
+  })
+</script>
