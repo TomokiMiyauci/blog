01e300bdf5024ab8292114ce0144e658c7719d6a
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-08-22 22:16:28 +0900

    :lipstick: (ButtonCircle.vue) Apply disabled style

--- packages/client/src/components/atoms/buttons/ButtonCircle.vue
+++ packages/client/src/components/atoms/buttons/ButtonCircle.vue
@@ -1,6 +1,6 @@
 <template>
   <base-button
-    class="p-3 focus:outline-none rounded-full items-center hover:bg-gray-100 dark-hover:bg-gray-900 transition duration-300 justify-center inline-flex"
+    class="p-3 focus:outline-none rounded-full items-center hover:bg-gray-100 dark-hover:bg-gray-900 transition duration-500 justify-center inline-flex disabled:cursor-not-allowed disabled:text-gray-500 disabled:bg-transparent"
     v-bind="$attrs"
     v-on="$listeners"
   >
