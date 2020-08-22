fdfcef6f13d52b04338055aed60c6a6220ca5491
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-08-23 01:53:34 +0900

    :sparkles: (snackbar) Add base snackbar component

---
+++ packages/client/src/components/molecules/BaseSnackbar.vue
@@ -0,0 +1,61 @@
+<template>
+  <transition name="fade-left">
+    <div
+      v-show="isOpen"
+      class="flex items-center hover:shadow-lg transition-shadow duration-700 rounded-full shadow dark:shadow-lg p-1 pl-5"
+    >
+      <slot name="icon" />
+      <div class="ml-3">
+        <slot name="text" />
+      </div>
+      <lazy-button-close
+        class="ml-10 bg-white hover:bg-teal-500 dark:bg-gray-800 dark-hover:bg-teal-500"
+        @click="close"
+      />
+    </div>
+  </transition>
+</template>
+
+<script lang="ts">
+  import { defineComponent, ref, watch } from 'nuxt-composition-api'
+
+  const useAutoClose = (milliseconeds: number) => {
+    const isOpen = ref(false)
+    const timeoutId = ref<NodeJS.Timeout>()
+
+    const open = (): void => {
+      isOpen.value = true
+    }
+
+    const close = (): void => {
+      if (timeoutId.value) {
+        clearTimeout(timeoutId.value)
+      }
+      isOpen.value = false
+    }
+
+    watch(isOpen, (now, prev) => {
+      if (now && !prev) {
+        timeoutId.value = setTimeout(() => {
+          isOpen.value = false
+          if (timeoutId.value) {
+            clearTimeout(timeoutId.value)
+          }
+        }, milliseconeds)
+      }
+    })
+
+    return { isOpen, open, close, timeoutId }
+  }
+  export default defineComponent({
+    props: {
+      milliseconeds: {
+        type: Number,
+        default: 5000
+      }
+    },
+    setup(props) {
+      return useAutoClose(props.milliseconeds)
+    }
+  })
+</script>
---
+++ packages/client/src/components/molecules/snackbar/SnackbarSuccess.vue
@@ -0,0 +1,28 @@
+<template>
+  <lazy-base-snackbar ref="snackbar" class="bg-gray-800 dark:bg-white">
+    <template #icon>
+      <lazy-mdi-check-circle class="text-teal-500" />
+    </template>
+    <template #text>
+      <span class="text-white dark:text-teal-900">Success! Posted</span>
+    </template>
+  </lazy-base-snackbar>
+</template>
+
+<script lang="ts">
+  import type BaseSnackbar from '@/components/molecules/BaseSnackbar.vue'
+  import { defineComponent, ref } from 'nuxt-composition-api'
+  export default defineComponent({
+    setup() {
+      const snackbar = ref<InstanceType<typeof BaseSnackbar>>()
+
+      return {
+        snackbar,
+        open: () => {
+          if (!snackbar.value) return
+          snackbar.value.open()
+        }
+      }
+    }
+  })
+</script>
