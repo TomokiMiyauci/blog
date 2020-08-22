c1aeac299d185a33511e1137d0b06619631ae20f
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-08-23 01:54:44 +0900

    :sparkles: (ArticleComment.vue) Add checking postable or not

--- packages/client/src/components/organisms/ArticleComment.vue
+++ packages/client/src/components/organisms/ArticleComment.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
-    <h2 class="mb-5 text-2xl"><mdi-comment-text-multiple /><span class="ml-2">Comments</span></h2>
-    <!-- <p>コミュニティガイドラインに沿ってコメント</p> -->
-    <div class="flex">
-      <mdi-account width="48" height="48" class="p-3 bg-gray-300 rounded-md shadow" />
-      <textarea-comment v-model="newCommentRef" class="ml-2" />
-      <lazy-button-send :disabled="!newCommentRef" @click="onSend" />
+    <h2 class="mb-5 text-2xl">
+      <lazy-mdi-comment-text-multiple /><span class="ml-2">Comments</span
+      ><span class="px-2 shadow ml-2 rounded-full bg-red-500">{{ commentCount }}</span>
+    </h2>
+    <portal to="notice">
+      <lazy-snackbar-success ref="snackbar" />
+    </portal>
+    <div class="flex items-start">
+      <lazy-mdi-account width="48" height="48" class="p-3 bg-gray-300 rounded-md shadow dark:text-teal-900" />
+      <lazy-textarea-comment v-model="newCommentRef" class="ml-2" />
+      <lazy-button-send :disabled="!isPostable" @click="onSend" />
     </div>
 
     <lazy-comment-list :comments="commentsRef" class="mt-8" />
@@ -13,13 +18,15 @@
 </template>
 
 <script lang="ts">
+  import type BaseSnackbar from '@/components/molecules/BaseSnackbar.vue'
   import { Comment } from '@/types/firestore'
-  import { defineComponent, ref } from 'nuxt-composition-api'
+  import { defineComponent, ref, computed } from 'nuxt-composition-api'
 
   const useComment = () => {
     const commentsRef = ref<Comment[]>([])
     const newCommentRef = ref<string>('')
-    const onSend = () => {
+    const onSend = (): void => {
+      if (!isPostable.value) return
       commentsRef.value = [
         ...commentsRef.value,
         { id: (commentsRef.value.length + 1).toString(), createdAt: new Date(), value: newCommentRef.value }
@@ -27,11 +34,36 @@
       newCommentRef.value = ''
     }
 
-    return { onSend, commentsRef, newCommentRef }
+    const isPostable = computed<boolean>(() => {
+      const result = newCommentRef.value.trim().match(/\S/g)
+
+      return !!result && !!result.length
+    })
+
+    const commentCount = computed(() => {
+      return commentsRef.value.length
+    })
+
+    return { onSend, commentsRef, newCommentRef, isPostable, commentCount }
   }
   export default defineComponent({
     setup() {
-      return { ...useComment() }
+      const { onSend, commentsRef, newCommentRef, isPostable, commentCount } = useComment()
+
+      const snackbar = ref<InstanceType<typeof BaseSnackbar>>()
+
+      return {
+        commentsRef,
+        newCommentRef,
+        isPostable,
+        snackbar,
+        commentCount,
+        onSend: () => {
+          onSend()
+          if (!snackbar.value) return
+          snackbar.value.open()
+        }
+      }
     }
   })
 </script>
