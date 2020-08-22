98f04f11f85b1c4f26944066ce531377fd77b705
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-08-22 10:39:18 +0900

    :label: (auth.ts) Extend types of Context

--- packages/client/src/middleware/auth.ts
+++ packages/client/src/middleware/auth.ts
@@ -1,9 +1,28 @@
 import { user as userStore } from '@/store'
+import type firebase from 'firebase'
 import { defineNuxtMiddleware } from 'nuxt-composition-api'
 
 declare module '@nuxt/types' {
   interface Context {
+    $fireStore: firebase.firestore.Firestore
+    $fireStoreObj: typeof firebase.firestore
+    $fireDb: firebase.database.Database
+    $fireDbObj: typeof firebase.database
+    $fireFunc: firebase.functions.Functions
+    $fireFuncObj: typeof firebase.functions
+    $fireStorage: firebase.storage.Storage
+    $fireStorageObj: typeof firebase.storage
     $fireAuth: firebase.auth.Auth
+    $fireAuthObj: typeof firebase.auth
+    $fireAuthUnsubscribe: firebase.Unsubscribe
+    $fireMess: firebase.messaging.Messaging
+    $fireMessObj: typeof firebase.messaging
+    $fireAnalytics: firebase.analytics.Analytics
+    $fireAnalyticsObj: typeof firebase.analytics
+    $firePerf: firebase.performance.Performance
+    $firePerfObj: typeof firebase.performance
+    $fireConfig: firebase.remoteConfig.RemoteConfig
+    $fireConfigObj: typeof firebase.remoteConfig
   }
 }
 
