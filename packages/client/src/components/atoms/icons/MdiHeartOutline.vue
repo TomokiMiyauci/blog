7b97b9ae899c9d0216453d274e2814d83d4ae10a
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-08-22 10:03:25 +0900

    :sparkles: (MdiHeartOutline.vue) Add new icon

---
+++ packages/client/src/components/atoms/icons/MdiHeartOutline.vue
@@ -0,0 +1,16 @@
+<template>
+  <lazy-base-svg v-bind="$attrs">
+    <path :d="mdiHeartOutline" />
+  </lazy-base-svg>
+</template>
+
+<script lang="ts">
+  import { mdiHeartOutline } from '@mdi/js'
+  import { defineComponent } from 'nuxt-composition-api'
+
+  export default defineComponent({
+    setup() {
+      return { mdiHeartOutline }
+    }
+  })
+</script>
