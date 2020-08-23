0bce1fcf8aff03ef1ac7767b5b76d459500f0dc2
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-08-23 13:20:38 +0900

    :white_check_mark: (articles.spec.ts) Add article comment test

--- packages/firebase/firestore/test/articles.spec.ts
+++ packages/firebase/firestore/test/articles.spec.ts
@@ -4,6 +4,25 @@ import * as firebase from '@firebase/testing'
 
 import { PROJECT_ID } from './'
 
+const authApp = () => {
+  return firebase
+    .initializeTestApp({
+      projectId: PROJECT_ID,
+      auth: { uid: 'admin', name: 'admin' }
+    })
+    .firestore()
+}
+
+const unauthApp = () => {
+  return firebase
+    .initializeTestApp({
+      projectId: PROJECT_ID
+    })
+    .firestore()
+}
+
+const timestamp = firebase.firestore.FieldValue.serverTimestamp()
+
 describe('firestore', () => {
   beforeAll(async () => {
     await firebase.loadFirestoreRules({
@@ -76,6 +95,93 @@ describe('firestore', () => {
             like: 0
           })
         )
+      })
+
+      describe('comments', () => {
+        describe('UNAUTH', () => {
+          const firestore = unauthApp()
+          const commentRef = firestore.collection('articles').doc('slug').collection('comments')
+          const commentDoc = firestore.collection('articles').doc('slug').collection('comments').doc('comment')
+
+          it('[LIST: OK]', () => {
+            firebase.assertSucceeds(commentRef.get())
+          })
+
+          it('[GET: OK]', () => {
+            firebase.assertSucceeds(commentDoc.get())
+          })
+
+          it('[POST: NG]', () => {
+            firebase.assertFails(
+              commentDoc.set({
+                text: 'comment',
+                userRef: commentDoc,
+                createdAt: timestamp
+              })
+            )
+          })
+
+          it('[DELETE: NG]', () => {
+            firebase.assertFails(commentDoc.delete())
+          })
+        })
+
+        describe('AUTH', () => {
+          const firestore = authApp()
+          const commentRef = firestore.collection('articles').doc('slug').collection('comments')
+          const commentDoc = firestore.collection('articles').doc('slug').collection('comments').doc('comment')
+
+          describe('[POST]', () => {
+            it('[NG]', () => {
+              firebase.assertFails(commentRef.add({}))
+            })
+
+            it('[NG]text:any', () => {
+              firebase.assertFails(
+                commentRef.add({
+                  text: 0,
+                  userRef: commentDoc,
+                  createdAt: timestamp
+                })
+              )
+            })
+
+            it('[NG]>3', () => {
+              firebase.assertFails(
+                commentRef.add({
+                  text: 'comment',
+                  userRef: commentDoc,
+                  createdAt: timestamp,
+                  hoge: 1
+                })
+              )
+            })
+
+            it('[OK]text:string, createdAt:timestamp', () => {
+              firebase.assertSucceeds(
+                commentRef.add({
+                  text: 'comment',
+                  userRef: commentDoc,
+                  createdAt: timestamp
+                })
+              )
+            })
+          })
+
+          it('[UPDATE: NG]', () => {
+            firebase.assertFails(
+              commentDoc.update({
+                text: 'comment',
+                userRef: commentDoc,
+                createdAt: timestamp
+              })
+            )
+          })
+
+          it('[DELETE: OK]', () => {
+            firebase.assertSucceeds(commentDoc.delete())
+          })
+        })
       })
 
       describe('likedUsers/', () => {
