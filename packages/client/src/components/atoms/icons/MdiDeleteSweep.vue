7143833c44d730c2c68111400583ad20b7d9cc4a
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-08-24 00:54:07 +0900

    :sparkles: Add delete icon and button

---
+++ packages/client/src/components/atoms/icons/MdiDeleteSweep.vue
@@ -0,0 +1,16 @@
+<template>
+  <lazy-base-svg v-bind="$attrs">
+    <path :d="mdiDeleteSweep" />
+  </lazy-base-svg>
+</template>
+
+<script lang="ts">
+  import { mdiDeleteSweep } from '@mdi/js'
+  import { defineComponent } from 'nuxt-composition-api'
+
+  export default defineComponent({
+    setup() {
+      return { mdiDeleteSweep }
+    }
+  })
+</script>
