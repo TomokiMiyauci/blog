b3d347be951f9a84f453bfc3128ba267a442d78a
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-08-23 16:39:57 +0900

    :sparkles: (onCreate.ts) Add slack notice on comment posted

--- packages/firebase/functions/src/article/comment/onCreate.ts
+++ packages/firebase/functions/src/article/comment/onCreate.ts
@@ -1,10 +1,14 @@
 import * as admin from 'firebase-admin'
 import * as functions from 'firebase-functions'
 
+import { IncomingWebhook } from '@slack/webhook'
+const webhook = new IncomingWebhook(functions.config().slack.url)
+
 export default functions
   .region('asia-northeast1')
   .firestore.document('articles/{slug}/comments/{comment}')
-  .onCreate((snapshot) => {
+  .onCreate(async (snapshot, { auth }) => {
+    const text = snapshot.data().text as string
     const articleRef = snapshot.ref.parent.parent
 
     console.log(111111111111111111)
@@ -17,5 +21,13 @@ export default functions
       comment: admin.firestore.FieldValue.increment(1)
     })
 
-    return batch.commit()
+    await batch.commit()
+    const message = {
+      icon_emoji: ':simple_smile:',
+      username: auth.uid,
+      text: `${articleRef.id}
+${text}`
+    }
+
+    return webhook.send(message)
   })
