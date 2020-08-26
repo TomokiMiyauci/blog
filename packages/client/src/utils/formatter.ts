66852881592e67a26d62a12e3de9d1f0e389c6f3
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-08-27 02:28:03 +0900

    :bug: (formatter.ts) Double assertion for avoid type error

--- packages/client/src/utils/formatter.ts
+++ packages/client/src/utils/formatter.ts
@@ -1,8 +1,12 @@
+import type firebase from 'firebase'
+
 export const formatDate = (date: Date, locale: string = 'en'): string => {
   const options = { year: 'numeric', month: 'long', day: 'numeric' }
   return new Date(date).toLocaleDateString(locale, options)
 }
 
-export const timestamp2Date = (timestamp: firebase.firestore.Timestamp): Date => {
-  return timestamp.toDate()
+export const timestamp2Date = (timestamp: firebase.firestore.Timestamp | firebase.firestore.FieldValue): Date => {
+  const t = (timestamp as any) as firebase.firestore.Timestamp
+
+  return t.toDate()
 }
