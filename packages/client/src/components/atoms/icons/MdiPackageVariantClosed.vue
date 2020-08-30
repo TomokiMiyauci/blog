dc68a69e3da373196537e7df659cf47f6edeaf41
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-08-30 22:09:11 +0900

    :sparkles: Add new icon and tag

---
+++ packages/client/src/components/atoms/icons/MdiPackageVariantClosed.vue
@@ -0,0 +1,16 @@
+<template>
+  <lazy-base-svg class="text-purple-700" v-bind="$attrs">
+    <path :d="mdiPackageVariantClosed" />
+  </lazy-base-svg>
+</template>
+
+<script lang="ts">
+  import { mdiPackageVariantClosed } from '@mdi/js'
+  import { defineComponent } from 'nuxt-composition-api'
+
+  export default defineComponent({
+    setup() {
+      return { mdiPackageVariantClosed }
+    }
+  })
+</script>
---
+++ packages/client/src/components/atoms/tags/TagCompositionApi.vue
@@ -0,0 +1,11 @@
+<template>
+  <lazy-base-tag name="composition-api">
+    <MdiPackageVariantClosed />
+  </lazy-base-tag>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'nuxt-composition-api'
+
+  export default defineComponent({})
+</script>
