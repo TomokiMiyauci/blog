e716314183f9c66ece2a6bbae3f2f6a840bf1524
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-09-03 02:06:44 +0900

    :wrench: (preview.ts) Add global locale toolbar

--- packages/client/storybook/preview.ts
+++ packages/client/storybook/preview.ts
@@ -1,1 +1,34 @@
 import '~~/.nuxt-storybook/storybook/preview.js'
+import Vue from 'vue'
+import VueI18n from 'vue-i18n'
+
+Vue.use(VueI18n)
+const i18n = new VueI18n({
+  locale: 'en'
+})
+
+export const globalTypes = {
+  locale: {
+    name: 'locale',
+    description: 'i18n',
+    defaultValue: 'en',
+    toolbar: {
+      icon: 'globe',
+      items: [
+        { value: 'en', right: '🇺🇸', title: 'English' },
+        { value: 'ja', right: '🇯🇵', title: '日本語' }
+      ]
+    }
+  }
+}
+
+const withLocale = (_: any, { globals }: any) => {
+  i18n.locale = globals.locale
+
+  return {
+    template: '<story />',
+    i18n
+  }
+}
+
+export const decorators = [withLocale]
