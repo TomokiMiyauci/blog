e4823bb0c102df5288e220be07a9d722b0fda674
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-09-02 20:56:38 +0900

    :wrench: Add storybook config

---
+++ packages/client/storybook.config.ts
@@ -0,0 +1,5 @@
+export default {
+  srcDir: 'src',
+  components: true,
+  buildModules: ['@nuxt/typescript-build', '@nuxtjs/tailwindcss', '@nuxtjs/composition-api']
+}
---
+++ packages/client/storybook/main.ts
@@ -0,0 +1,19 @@
+const path = require('path')
+
+module.exports = {
+  webpackFinal(config: any, options: any) {
+    config = options.nuxtStorybookConfig.webpackFinal(config, options)
+    // extend config
+    return config
+  },
+  stories: [path.join(__dirname, '..', 'stories', '**', '*.stories.@(ts|js)')],
+  addons: [
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        actions: false
+      }
+    },
+    '@storybook/addon-knobs'
+  ]
+}
