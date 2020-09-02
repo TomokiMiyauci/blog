eb8de1cfa01d0eae79ba48f8847b1d024742087c
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-09-03 02:30:32 +0900

    :recycle: (CalendarEditDate.stories.ts) Remove i18n props

--- packages/client/stories/atoms/icontexts/CalendarEditDate.stories.ts
+++ packages/client/stories/atoms/icontexts/CalendarEditDate.stories.ts
@@ -1,44 +1,17 @@
-import { select } from '@storybook/addon-knobs'
-import { addDecorator } from '@storybook/vue'
-import Vue from 'vue'
-import VueI18n from 'vue-i18n'
-Vue.use(VueI18n)
-
-const i18n = new VueI18n()
-
 export default {
-  title: 'CalendarEditDate'
+  title: 'CalendarEditDate',
+  argTypes: {
+    date: { control: 'text', description: 'Date or String' }
+  }
 }
 
-addDecorator(() => ({
-  template: '<story/>',
-  i18n,
-  props: {
-    storybookLocale: {
-      type: String,
-      default: select('locale', ['en', 'ja'], 'en')
-    }
-  },
-  watch: {
-    storybookLocale: {
-      handler(a: any) {
-        i18n.locale = a
-      },
-      immediate: true
-    }
-  }
-}))
-
-i18n.locale = 'en'
-
 export const DEFAULT = () => ({
-  template: '<calendar-edit-date :text="text" label="label" />',
+  template: '<calendar-edit-date :date="date" />',
   props: {
     date: {
       default: new Date()
-    },
-    label: {
-      default: select('hoge', ['hogehoge'], 'hogehoge')
     }
   }
 })
+
+DEFAULT.args = { date: new Date() }
