e536a96d94c7c8bdedd383da8c97698380bb93b5
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-09-03 22:21:05 +0900

    :globe_with_meridians: Apply i18n

--- packages/client/src/components/atoms/icontexts/TimerReadingTime.vue
+++ packages/client/src/components/atoms/icontexts/TimerReadingTime.vue
@@ -7,10 +7,6 @@
 <script lang="ts">
   import { defineComponent, computed } from '@nuxtjs/composition-api'
 
-  // const useTimePrefix = () => {
-  //   const ct = useContext()
-  //   ct.app.i18n.locale
-  // }
   export default defineComponent({
     props: {
       text: {
@@ -19,20 +15,19 @@
       }
     },
 
-    setup(props) {
+    setup(props, { root }) {
       const minuteText = computed(() => {
-        // switch (root.$i18n.locale) {
-        //   case 'en': {
-        //     return
-        //   }
-        // }
+        const minute = props.text.split(' ')[0]
+        switch (root.$i18n.locale) {
+          case 'ja': {
+            return `${minute} ${root.$t('UNIT.MINUTE')}`
+          }
 
-        return `${props.text}min`
-        // if (root.$i18n.locale === 'en') {
-        //   return `${props.text}min`
-        // } else if (root.$i18n.locale === 'ja') {
-        //   return `${props.text}分`
-        // }
+          default: {
+            const suffix = Number(minute) > 1 ? root.$t('UNIT.MINUTE.MULTIPLE') : root.$t('UNIT.MINUTE.SINGULAR')
+            return `${minute} ${suffix}`
+          }
+        }
       })
 
       return { minuteText }
