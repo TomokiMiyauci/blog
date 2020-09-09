5e299b8ff3a6664e3703377b947c6fed451a2ad8
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-09-09 22:45:33 +0900

    :seedling: (MdiBookmark.vue) Add new icon

---
+++ packages/client/src/components/atoms/icons/MdiBookmark.vue
@@ -0,0 +1,16 @@
+<template>
+  <base-svg v-bind="$attrs">
+    <path :d="mdiBookmark" />
+  </base-svg>
+</template>
+
+<script lang="ts">
+  import { mdiBookmark } from '@mdi/js'
+  import { defineComponent } from '@nuxtjs/composition-api'
+
+  export default defineComponent({
+    setup() {
+      return { mdiBookmark }
+    }
+  })
+</script>
