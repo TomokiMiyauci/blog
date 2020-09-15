6a9cec4e1d52899f8c37e6c918e8f7235a45e685
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-09-16 03:15:52 +0900

    :sparkles: (Messenger.stories.ts) Add messenger story

--- packages/client/stories/molecules/Messenger.stories.ts
+++ packages/client/stories/molecules/Messenger.stories.ts
@@ -1,7 +1,32 @@
 export default {
-  title: 'Messenger'
+  title: 'Messenger',
+
+  argTypes: {
+    step: {
+      control: {
+        type: 'inline-radio',
+        options: ['other', 'entrance']
+      }
+    },
+
+    isLogin: {
+      control: {
+        type: 'boolean'
+      }
+    }
+  }
 }
 
 export const DEFAULT = () => ({
-  template: '<messenger />'
+  template: '<messenger v-bind="$props" />',
+
+  props: {
+    step: {
+      default: 'entrance'
+    },
+
+    isLogin: {
+      default: false
+    }
+  }
 })
