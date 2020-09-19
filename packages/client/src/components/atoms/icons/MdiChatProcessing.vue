1e537f9a47925227fdf35f61b8fd14d5a7cf9b71
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-09-19 17:36:05 +0900

    :sparkles: (MdiChatProcessing) Add icon

---
+++ packages/client/src/components/atoms/icons/MdiChatProcessing.vue
@@ -0,0 +1,16 @@
+<template>
+  <base-svg>
+    <path :d="mdiChatProcessing" />
+  </base-svg>
+</template>
+
+<script lang="ts">
+  import { mdiChatProcessing } from '@mdi/js'
+  import { defineComponent } from '@nuxtjs/composition-api'
+
+  export default defineComponent({
+    setup() {
+      return { mdiChatProcessing }
+    }
+  })
+</script>
