9a27a913c1c58d612e4a0618d3ebe674e74f7929
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-10-04 14:50:51 +0900

    :recycle: (lang-switcher/*) Split isolated compon…

---
+++ packages/client/src/components/lang-switcher/atoms/Japanese.vue
@@ -0,0 +1,31 @@
+<template>
+  <nuxt-link
+    class="cursor-pointer px-2 py-1 flex rounded items-center bg-gradient-to-r hover:from-teal-300 hover:to-green-500 dark-hover:bg-green-600 transition-all duration-300"
+    v-bind="$attrs"
+    :to="switchLocalePath('ja')"
+    active-class="bg-gradient-to-r from-red-400 to-orange-200"
+    exact
+    replace
+  >
+    <flag-for-japan height="36" /><span class="ml-1">{{ $t('japanese') }}</span>
+  </nuxt-link>
+</template>
+
+<script lang="ts">
+  import FlagForJapan from '@/components/atoms/icons/FlagForJapan.vue'
+  import { defineComponent } from '@nuxtjs/composition-api'
+
+  export default defineComponent({
+    components: {
+      FlagForJapan
+    }
+  })
+</script>
+
+<i18n lang="yml">
+en:
+  japanese: 日本語
+
+ja:
+  japanese: 日本語
+</i18n>
---
+++ packages/client/stories/lang-switcher/atoms/English.stories.ts
@@ -0,0 +1,10 @@
+import English from '@/components/lang-switcher/atoms/English.vue'
+
+export default {
+  title: 'lang-switcher/atoms/English',
+  components: English
+}
+
+export const DEFAULT = () => ({
+  template: '<english />'
+})
