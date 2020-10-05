6746bb532de795f3a4f181cb7bb979522b5976e8
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-10-05 23:21:11 +0900

    :truck: (AppFooter) Move to app dir

---
+++ packages/client/stories/app/molecules/AppFooter.stories.ts
@@ -0,0 +1,10 @@
+import AppFooter from '@/components/app/molecules/AppFooter.vue'
+
+export default {
+  title: 'app/molecules/AppFooter',
+  components: AppFooter
+}
+
+export const DEFAULT = () => ({
+  template: '<app-footer />'
+})
