c5af741b4bc4d274eedf4e8bbf076631b6a010be
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-10-08 01:27:27 +0900

    :lipstick: Use tooltip

--- packages/client/src/components/atoms/buttons/ButtonDelete.vue
+++ packages/client/src/components/atoms/buttons/ButtonDelete.vue
@@ -1,10 +1,17 @@
 <template>
-  <button-circle v-bind="$attrs" v-on="$listeners">
-    <mdi-delete-sweep />
-  </button-circle>
+  <base-tooltip>
+    <template #activator>
+      <button-circle v-bind="$attrs" v-on="$listeners">
+        <mdi-delete-sweep />
+      </button-circle>
+    </template>
+
+    <span>{{ $t('tooltip') }}</span>
+  </base-tooltip>
 </template>
 
 <script lang="ts">
+  import BaseTooltip from '@/components/atoms/BaseTooltip.vue'
   import ButtonCircle from '@/components/atoms/buttons/ButtonCircle.vue'
   import MdiDeleteSweep from '@/components/atoms/icons/MdiDeleteSweep.vue'
   import { defineComponent } from '@nuxtjs/composition-api'
@@ -12,7 +19,15 @@
   export default defineComponent({
     components: {
       ButtonCircle,
+      BaseTooltip,
       MdiDeleteSweep
     }
   })
 </script>
+
+<i18n lang="yml">
+en:
+  tooltip: Delete
+ja:
+  tooltip: 削除
+</i18n>
