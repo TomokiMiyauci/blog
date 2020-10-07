0f713280b0fe0adea6216e4a5b5fa38353c2338e
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-10-08 00:15:24 +0900

    :sparkles: Add base components

---
+++ packages/client/src/components/atoms/BaseCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="whitespace-no-wrap p-1">
+    <slot name="header">
+      <slot name="title" />
+    </slot>
+
+    <hr class="mt-1 mb-2" />
+
+    <div class="px-4">
+      <slot name="body" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from '@nuxtjs/composition-api'
+  export default defineComponent({})
+</script>
---
+++ packages/client/src/components/atoms/buttons/ButtonSquare.vue
@@ -0,0 +1,20 @@
+<template>
+  <base-button
+    class="p-3 focus:outline-none rounded-md items-center hover:bg-gray-100 dark-hover:bg-gray-700 dark-disabled:bg-transparent transition duration-500 justify-center inline-flex disabled:cursor-not-allowed disabled:text-gray-500 disabled:bg-transparent"
+    v-bind="$attrs"
+    v-on="$listeners"
+  >
+    <slot />
+  </base-button>
+</template>
+
+<script lang="ts">
+  import BaseButton from '@/components/atoms/BaseButton.vue'
+  import { defineComponent } from '@nuxtjs/composition-api'
+
+  export default defineComponent({
+    components: {
+      BaseButton
+    }
+  })
+</script>
