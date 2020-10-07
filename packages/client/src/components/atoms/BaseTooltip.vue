f201ddce462bd16b085c88399ebf1107377961ef
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-10-08 01:26:22 +0900

    :sparkles: (BaseTooltip.vue) Add direction

--- packages/client/src/components/atoms/BaseTooltip.vue
+++ packages/client/src/components/atoms/BaseTooltip.vue
@@ -1,10 +1,11 @@
 <template>
-  <span class="relative" @mouseover="on" @focus="on" @mouseleave="off" @blur="off">
+  <span class="relative" @mouseenter="on" @mouseleave="off" @blur="off" @focus="on">
     <slot name="activator" />
     <transition name="scale-in">
       <span
         v-show="state"
-        class="text-white absolute right-0 -mb-12 whitespace-no-wrap bottom-0 bg-gray-700 bg-opacity-75 rounded-md p-2 shadow"
+        class="text-white absolute right-0 whitespace-no-wrap bg-gray-700 bg-opacity-75 rounded-md p-2 shadow"
+        :class="[yDirection]"
       >
         <slot />
       </span>
@@ -14,11 +15,38 @@
 
 <script lang="ts">
   import useSwitch from '@/core/switch'
-  import { defineComponent } from '@nuxtjs/composition-api'
+  import { defineComponent, toRefs, computed } from '@nuxtjs/composition-api'
+  import { Ref } from '@vue/composition-api'
+
+  const useDirection = (direction: { top: Ref<boolean>; bottom: Ref<boolean> }) => {
+    const { top, bottom } = direction
+
+    const yDirection = computed(() => {
+      if (top.value) {
+        return 'top-0 -mt-12'
+      } else if (bottom.value) {
+        return 'bottom-0 -mb-12'
+      }
+    })
+
+    return { yDirection }
+  }
 
   export default defineComponent({
-    setup() {
-      return useSwitch()
+    props: {
+      top: {
+        type: Boolean,
+        default: false
+      },
+
+      bottom: {
+        type: Boolean,
+        default: true
+      }
+    },
+
+    setup(props) {
+      return { ...useSwitch(), ...useDirection(toRefs(props)) }
     }
   })
 </script>
