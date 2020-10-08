b494aea5e6550e78b4547f7db563a606be7f7de7
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-10-08 21:36:42 +0900

    :sparkles: (reading-progress/*) Add reading progress feature

---
+++ packages/client/src/components/reading-progress/atoms/BaseProgress.vue
@@ -0,0 +1,41 @@
+<template>
+  <progress
+    class="h-1 border-none bg-transparent transition duration-300 appearance-none w-full"
+    :max="max"
+    :value="value"
+  />
+</template>
+
+<script lang="ts">
+  import { defineComponent } from '@nuxtjs/composition-api'
+
+  export default defineComponent({
+    props: {
+      max: {
+        type: [String, Number],
+        default: 0
+      },
+
+      value: {
+        type: [String, Number],
+        default: 0
+      }
+    }
+  })
+</script>
+
+<style lang="scss">
+  progress[value] {
+    &::-webkit-progress-bar {
+      background-color: transparent;
+    }
+
+    &::-webkit-progress-value {
+      @apply bg-gradient-to-r to-green-400 from-teal-700;
+    }
+
+    &::-moz-progress-bar {
+      @apply bg-gradient-to-r to-green-400 from-teal-700;
+    }
+  }
+</style>
