ab55bdcb477ef54770265e5dba9093756d7f4972
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-10-22 00:52:56 +0900

    :recycle: (AppBar.vue) Use logo text

--- packages/client/src/components/app/molecules/AppBar.vue
+++ packages/client/src/components/app/molecules/AppBar.vue
@@ -5,12 +5,8 @@
     <div class="container relative h-full flex items-center justify-between mx-auto px-4 lg:py-6">
       <nuxt-link class="font-mono inline-flex items-end" :to="localePath('/')">
         <img width="56px" src="/icon.png" alt="logo" />
-        <span class="ml-2 text-3xl leading-none tracking-tighter lg:text-4xl text-teal-900 dark:text-white"
-          ><span style="color: #72deaf">{{ t }}</span
-          >{{ ech }}<span style="color: #72deaf">{{ s }}</span
-          >{{ rc }}</span
-        ></nuxt-link
-      >
+        <logo-text class="hidden sm:block" />
+      </nuxt-link>
       <div class="flex items-center">
         <fulltext-search />
         <lang-switcher class="hidden ml-2 lg:block" />
@@ -30,18 +26,16 @@
   import AppNavigationDrawer from '@/components/app/molecules/AppNavigationDrawer.vue'
   import ButtonCircle from '@/components/atoms/buttons/ButtonCircle.vue'
   import HamburgerClose from '@/components/atoms/icons/HamburgerClose.vue'
+  import LogoText from '@/components/atoms/logos/LogoText.vue'
   import LangSwitcher from '@/components/lang-switcher/molecules/LangSwitcher.vue'
   import FulltextSearch from '@/components/searcher/molecules/FulltextSearch.vue'
   import { defineComponent, ref } from '@nuxtjs/composition-api'
-  const t = 'T'
-  const ech = 'ech'
-  const s = 's'
-  const rc = 'rc'
 
   export default defineComponent({
     components: {
       ButtonCircle,
       FulltextSearch,
+      LogoText,
       LangSwitcher,
       HamburgerClose,
       AppNavigationDrawer
@@ -49,7 +43,7 @@
 
     setup() {
       const isActive = ref(false)
-      return { isActive, t, ech, s, rc }
+      return { isActive }
     }
   })
 </script>
