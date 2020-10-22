3fb912c34bd7688360594035323c6541391f1dbd
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2020-10-23 00:35:59 +0900

    :sparkles: (formatter.ts) Add utils for format kin…

--- packages/client/src/utils/formatter.ts
+++ packages/client/src/utils/formatter.ts
@@ -10,3 +10,45 @@ export const timestamp2Date = (timestamp: firebase.firestore.FieldValue): Date =
   const t = timestamp as firebase.firestore.Timestamp
   return t.toDate()
 }
+
+/**
+ * @param string
+ * @return string camelCase
+ */
+const toCamelCase = (str: string): string => {
+  str = str.charAt(0).toLowerCase() + str.slice(1)
+  return str.replace(/[-_](.)/g, (match, group1) => {
+    return group1.toUpperCase()
+  })
+}
+
+/**
+ * @param string
+ * @return string snake_case
+ */
+export const toSnackCase = (str: string): string => {
+  const camel = toCamelCase(str)
+  return camel.replace(/[A-Z]/g, (s) => {
+    return '_' + s.charAt(0).toLowerCase()
+  })
+}
+
+/**
+ * @param string
+ * @return string kebab-case
+ */
+export const toKebabCase = (str: string): string => {
+  const camel = toCamelCase(str)
+  return camel.replace(/[A-Z]/g, (s) => {
+    return '-' + s.charAt(0).toLowerCase()
+  })
+}
+
+/**
+ * @param string
+ * @return string PascalCase
+ */
+export const toPascalCase = (str: string): string => {
+  const camel = toCamelCase(str)
+  return camel.charAt(0).toUpperCase() + camel.slice(1)
+}
